be6bbe05850eb7193d28800ec5e1b59572bb93c1
Tanmay <srivastava.tanmay.162003@gmail.com>
2023-01-03 14:26:58 +0530

    Initial Commit

---
+++ clonevideo/clonevideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  clonevideoApp.swift
+//  clonevideo
+//
+//  Created by Tanmay on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct clonevideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
